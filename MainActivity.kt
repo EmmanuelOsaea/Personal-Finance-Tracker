55f1ad7db705ebaa2c7d8c1a9a8e4f6df7dee2cf
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-22 05:27:50 +0100

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,1 +1,24 @@
+package com.example.financetracker
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.financetracker.databinding.ActivityMainBinding
+import com.example.financetracker.ui.TransactionViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: TransactionViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = TransactionViewModel(this)
+
+        // TODO: Display transaction list
+        // TODO: Add new transaction
+        // TODO: Delete transaction
+    }
+}
