350dc9d22899ea3393366a084e15a4859f6001bf
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-22 05:37:46 +0100

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -17,6 +17,32 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = TransactionViewModel(this)
 
+        // Inside onCreate()
+val adapter = TransactionAdapter(listOf())  // empty initially
+binding.rvTransactions.adapter = adapter
+
+binding.btnAddTransaction.setOnClickListener {
+    // Example: add a dummy transaction
+    val transaction = Transaction(
+        title = "Groceries",
+        amount = 50.0,
+        category = "Food",
+        date = System.currentTimeMillis()
+    )
+
+    // save using ViewModel
+    CoroutineScope(Dispatchers.IO).launch {
+        viewModel.addTransaction(transaction)
+
+        // refresh UI
+        val updatedList = viewModel.transactions.value ?: listOf()
+        withContext(Dispatchers.Main) {
+            binding.rvTransactions.adapter = TransactionAdapter(updatedList)
+        }
+    }
+}
+        
+        
         // TODO: Display transaction list
         // TODO: Add new transaction
         // TODO: Delete transaction
