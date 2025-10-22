881126f7f054d037785f41e3d1a7019017e91d6d
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-22 05:23:08 +0100

    Update AppDatabse.kt

--- AppDatabse.kt
+++ AppDatabse.kt
@@ -1,1 +1,10 @@
+package com.example.financetracker.db
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.financetracker.data.Transaction
+
+@Database(entities = [Transaction::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun transactionDao(): TransactionDao
+}
