dab70616ef64a0ab21faac0d9ea88958b49ba1c6
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-22 05:20:39 +0100

    Update TransactionRepository.kt

--- TransactionRepository.kt
+++ TransactionRepository.kt
@@ -1,1 +1,17 @@
+package com.example.financetracker.data
 
+import android.content.Context
+import androidx.room.Room
+import com.example.financetracker.db.AppDatabase
+
+class TransactionRepository(context: Context) {
+
+    private val db = Room.databaseBuilder(
+        context,
+        AppDatabase::class.java, "finance-db"
+    ).build()
+
+    suspend fun insert(transaction: Transaction) = db.transactionDao().insert(transaction)
+    suspend fun delete(transaction: Transaction) = db.transactionDao().delete(transaction)
+    suspend fun getAll() = db.transactionDao().getAllTransactions()
+}
