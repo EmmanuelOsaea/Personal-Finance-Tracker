a3cc175aae4e23c22fc6db623e67e95026116d52
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-22 05:24:53 +0100

    Update TransactionViewModel.kt

--- TransactionViewModel.kt
+++ TransactionViewModel.kt
@@ -1,1 +1,24 @@
+package com.example.financetracker.ui
 
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.financetracker.data.Transaction
+import com.example.financetracker.data.TransactionRepository
+import kotlinx.coroutines.Dispatchers
+
+class TransactionViewModel(context: Context) : ViewModel() {
+    private val repository = TransactionRepository(context)
+
+    val transactions = liveData(Dispatchers.IO) {
+        emit(repository.getAll())
+    }
+
+    suspend fun addTransaction(transaction: Transaction) {
+        repository.insert(transaction)
+    }
+
+    suspend fun removeTransaction(transaction: Transaction) {
+        repository.delete(transaction)
+    }
+}
