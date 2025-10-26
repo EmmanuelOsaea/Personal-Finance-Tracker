f964b49702b54e26399a99e01a2a4323d1d43147
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-26 18:42:13 +0100

    Update and rename AppDatabse.kt to AppDatabase.kt

---
+++ AppDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.financetracker.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.financetracker.data.Transaction
+import android.content.Context
+
+@Database(entities = [Transaction::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun transactionDao(): TransactionDao
+}
+
+
+companion object {
+        @Volatile private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "finance_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
